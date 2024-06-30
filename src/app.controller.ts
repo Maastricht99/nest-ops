9660f204ee82a55efbfde16e41280261b914ca10
Maastricht99 <19alexferrante99@gmail.com>
2024-06-30 14:15:55 +0200

    update pipeline

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getTest(): number {
+  getTest(): string {
     return this.appService.getTest();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,12 +5,7 @@ import { Env } from "./config";
 @Injectable()
 export class AppService {
 
-  constructor(
-    private readonly configService: ConfigService<Env>
-  ) {}
-
-  getTest(): number {
-    const { port } = this.configService.get("server", { infer: true })
-    return port;
+  getTest(): string {
+    return "Hello World";
   }
 }
