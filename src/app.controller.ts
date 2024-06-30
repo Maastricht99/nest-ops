903f46897709dfb4d9d5e3502be7de80e7e7647f
Maastricht99 <19alexferrante99@gmail.com>
2024-06-30 13:14:04 +0200

    add config loader

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getTest(): string {
+  getTest(): number {
     return this.appService.getTest();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { Env } from "./config";
 
 @Injectable()
 export class AppService {
 
   constructor(
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService<Env>
   ) {}
 
-  getTest(): string {
-    return this.configService.get("SERVER_PORT");
+  getTest(): number {
+    const { port } = this.configService.get("server", { infer: true })
+    return port;
   }
 }
